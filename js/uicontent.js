ff57430dad031351ba0c5132835ec1b5ad3d5275
GiraffeBot <TheGiraffe@users.noreply.github.com>
2018-02-09 09:55:30 -1000

    made the intro really nice - here's to procrastination!

--- js/uicontent.js
+++ js/uicontent.js
@@ -26,10 +26,11 @@ $('#syntaxhelp').on('click',function(){
       $('.helptext').hide();
       $('#hiddennav').css("margin-top","-30px");
   }else{
+      $('.hiddenoptions').hide()
     $('#syntaxlinks').show();
     $('.helptext').show();
     $('.helptexttitle').html("Click on a helpsheet to download it.");
-    $('.helptexttext').html("Why do we use Syntax Helpsheets? Programming isn't about the syntax or the exact commands you type into the text editor or IDE. ");
+    $('.helptexttext').html("Why do we use Syntax Helpsheets? Programming isn't about the syntax or the exact commands you type into the text editor or IDE. Your brain should be focused on how the code functions, as opposed to memorizing how the syntax looks. So please use these helpsheets to guide your programming, if necessary.");
     $('#hiddennav').css("margin-top","0px");
   }
 
@@ -37,20 +38,24 @@ $('#syntaxhelp').on('click',function(){
 });
 
 $(function(){
+
   $('.hiddenoptions').hide()
   $('#projectsbutton').on('click',function(){
   if($('.hiddenoptions').is(":visible")){
   $('.hiddenoptions').hide()
   }else{
   $('.hiddenoptions').show()
-
+  $('#syntaxlinks').hide();
+  $('.helptext').hide();
+  $('#hiddennav').css("margin-top","-30px");
   }
 
   });
 });
 
+
 //Previous and Next arrows functionality
-var welcometext=("Welcome to CodeKCS. Over the course of these four weeks, you will learn the basics of code from a creative perspective. We don't expect you to become a code wizard in four weeks - we don't even");
+var welcometext=("CodeKCS is a project-based curriculum for learning code. The idea here is that students often learn best through doing things themselves and by working on projects that mean something to them. There is also a mentorship aspect of this course. The idea of the self-taught programmer is inherently flawed, because mentors are a valuable part of the process of learning code. Mentors can prevent students from making inefficient mistakes and can pass along lots of advice to make the learning curve smoother. Through this course, students will be provided with guidance and frequent check-ins with the teacher to make sure they’re getting the most out of the curriculum. Should the students wish to further pursue programming after the end of the course, the teacher can get them in contact with more experienced mentors that can continue to guide them in this hobby or future career.<br>It is important to note that the purpose of this course is not to turn every student into an aspiring professional programmer or a future STEM major. It is simply to introduce the students to a potentially helpful skill that they can choose to use in the future to add to their existing interests.Coding at this level is like any skill such as writing, drawing, or running. You can have any of those skills as hobbies, but writing for fun every now and then does not mean you have to become a professional author someday. There are varying levels of commitment and detail to any hobby or skill. Hopefully this course will help you acquire a basic level of skill in programming that you can further build upon in the future if you wish.");
 $(function(){
   $('.lessoncontent').html(welcometext)
   $('#previous').on('click',function(){
