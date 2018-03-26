c2d5d7085b5725d2ebe0d932cca807407a312d77
GiraffeBot <TheGiraffe@users.noreply.github.com>
2018-03-26 01:14:46 -1000

    redesign 1

---
+++ js/homepagecontents.js
@@ -0,0 +1,16 @@
+//The hidden navbar, to navigate between lessons
+$(function (){
+  $('#homepageoptions1').hide();
+  $('#homepageoptions2').hide();
+  $('#parallax2').hide();
+  $('#parallax3').hide();
+  $('#learn').on('click',function(){
+      $('#homepageoptions1').show();
+      $('#parallax2').show();
+      $('#whattolearn').html('code');
+      document.getElementById("homepageoptions1").scrollIntoView({behavior:"smooth"});
+});
+$('#learnComputers').on('click',function(){
+    document.getElementById("parallax2").scrollIntoView({behavior:"smooth", block:"end"});
+});
+});
