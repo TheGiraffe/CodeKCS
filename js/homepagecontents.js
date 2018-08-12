3761eea0a34cf5c035776809ac57542bd9dc0476
GiraffeBot <TheGiraffe@users.noreply.github.com>
2018-08-12 11:13:22 -1000

    updated with lots of docs and information!

--- js/homepagecontents.js
+++ js/homepagecontents.js
@@ -41,13 +41,13 @@ $('#inquire').on('click',function(){
     $('#knowaboutoptions').html('<a title="The CodeKCS Project"><button class="cats" id="learnCodeKCS"><span>The CodeKCS Project</span></button></a><a title="Curriculum"><button class="cats" id="learnCurriculum"><span>The Class Curriculum</span></button></a><a title="CodeKCS Club"><button class="cats" id="learnCodeKCSClub"><span>The KCS Code Club</span></button></a>');
     $('#learnCodeKCS').on('click',function(){
         document.getElementById("parallax2").scrollIntoView({behavior:"smooth", block:"end"});
-        $('#learnAbout').show().html('The CodeKCS Project');
-        $('#learnAboutContent').show().html('Learn about the project and all the cool stuff we did for a semester!');
+        $('#learnAbout').show().html('Intro to the CodeKCS Project');
+        $('#learnAboutContent').show().html('<p>Check out this interview video made by Han Davis for the Akaku Ybeam program.It is a good introduction and background to the Code Kihei Charter project.</p><iframe width="560" height="315" src="https://www.youtube.com/embed/p5lorCKeFW0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><p><a href="http://seniorprojects.sophiaisfabulous.com/seniorproject.html">Click here</a> to learn more about the process of designing the project. <a href="https://thegiraffe.github.io/seniorprojectjournal/">Senior Project Journal</a></p>');
     });
     $('#learnCurriculum').on('click',function(){
         document.getElementById("parallax2").scrollIntoView({behavior:"smooth", block:"end"});
         $('#learnAbout').show().html('The Class Curriculum');
-        $('#learnAboutContent').show().html('Coming really soon. Have to compile the data');
+        $('#learnAboutContent').show().html('<p>This section is being updated with more curriculum ideas. For right now, here are some materials from the class:</p><a href="files/curriculumdocs/CodeKCSDay0.pdf"><button class="cats"><span>Scratch Assignment</span></button></a><br><a href="files/curriculumdocs/CodeKCSLesson2.pdf"><button class="cats"><span>HTML+CSS+Git</span></button></a><br><a href="files/Syllabus.pdf"><button class="cats"><span>Original Syllabus</span></button></a><br><a href="files/Pathways.pdf"><button class="cats"><span>Pathways idea from original brainstorming</span></button></a>');
     });
     $('#learnCodeKCSClub').on('click',function(){
         document.getElementById("parallax2").scrollIntoView({behavior:"smooth", block:"end"});
